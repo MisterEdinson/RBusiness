2e6c685bf46002f5512a4ffbfa00cc6ac57ddb0b
Edyard Karastoyka <88448179+MisterEdinson@users.noreply.github.com>
2024-09-23 13:05:09 +0300

    add home screen

--- app/src/main/java/com/skiffold/app/rbusiness/ui/splash/SplashFragment.kt
+++ app/src/main/java/com/skiffold/app/rbusiness/ui/splash/SplashFragment.kt
@@ -5,13 +5,26 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.fragment.findNavController
 import com.skiffold.app.rbusiness.R
+import com.skiffold.app.rbusiness.databinding.FragmentSplashBinding
 
 class SplashFragment : Fragment() {
+    private lateinit var binding: FragmentSplashBinding
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        return inflater.inflate(R.layout.fragment_splash, container, false)
+    ): View {
+        binding = FragmentSplashBinding.inflate(layoutInflater)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding.apply {
+            txtCreate.setOnClickListener {
+                findNavController().navigate(R.id.action_splashFragment_to_homeFragment)
+            }
+        }
     }
 }
