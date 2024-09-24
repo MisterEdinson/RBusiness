93602c61dcdb0d7049ef54cba69b21a17916510e
Edyard Karastoyka <88448179+MisterEdinson@users.noreply.github.com>
2024-09-24 13:55:38 +0300

    add education screen

--- app/src/main/java/com/skiffold/app/rbusiness/ui/job/JobFragment.kt
+++ app/src/main/java/com/skiffold/app/rbusiness/ui/job/JobFragment.kt
@@ -7,14 +7,16 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Toast
+import androidx.fragment.app.activityViewModels
 import com.skiffold.app.rbusiness.R
 import com.skiffold.app.rbusiness.databinding.FragmentJobBinding
+import com.skiffold.app.rbusiness.ui.home.GameViewModel
 import com.skiffold.app.rbusiness.ui.utils.DataGame
 
 class JobFragment : Fragment() {
     private lateinit var binding: FragmentJobBinding
+    private val viewModel: GameViewModel by activityViewModels()
     private lateinit var adapter: AdapterJob
-    private var dataSelectedJobs = DataGame.DATA_JOBS
     private var selectPos: Int = 0
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -27,19 +29,19 @@ class JobFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         initAdapter()
-        adapter.list.submitList(dataSelectedJobs)
     }
 
     private fun initAdapter() {
         adapter = AdapterJob{ click -> clickJob(click)}
         binding.rvJob.adapter = adapter
+        adapter.list.submitList(viewModel.dataSelectedJobs)
     }
 
     private fun clickJob(select: Int){
-        dataSelectedJobs.forEachIndexed { index, jobModel ->
+        viewModel.dataSelectedJobs.forEachIndexed { index, jobModel ->
             jobModel.selected = index == select
         }
-        adapter.list.submitList(dataSelectedJobs)
+        adapter.list.submitList(viewModel.dataSelectedJobs)
         adapter.notifyItemChanged(selectPos)
         selectPos = select
     }
