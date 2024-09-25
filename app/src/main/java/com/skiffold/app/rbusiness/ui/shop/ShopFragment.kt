0ec4216b7c8bd0909876a0cf685cc5f4604dac26
Edyard Karastoyka <88448179+MisterEdinson@users.noreply.github.com>
2024-09-25 11:25:32 +0300

    add screen

--- app/src/main/java/com/skiffold/app/rbusiness/ui/shop/ShopFragment.kt
+++ app/src/main/java/com/skiffold/app/rbusiness/ui/shop/ShopFragment.kt
@@ -6,12 +6,31 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.skiffold.app.rbusiness.R
+import com.skiffold.app.rbusiness.databinding.FragmentShopBinding
 
 class ShopFragment : Fragment() {
+    
+    private lateinit var binding: FragmentShopBinding
+    private lateinit var adapterShop: AdapterShop
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        return inflater.inflate(R.layout.fragment_shop, container, false)
+    ): View {
+        binding = FragmentShopBinding.inflate(layoutInflater)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        initAdapter()
+    }
+
+    private fun initAdapter() {
+        adapterShop = AdapterShop{ click -> selectProduct(click)}
+        binding.rvMarket.adapter = adapterShop
+    }
+
+    private fun selectProduct(click: Int) {
+
     }
 }
